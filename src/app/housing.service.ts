ae98c298884079acfe05f01fdda665d0d61c8186
Muhammad Sultan <muhammad.sultan@speridian.com>
2024-01-17 15:31:36 +0500

    Reactive and Template-driven forms

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -10,9 +10,9 @@ export class HousingService {
   protected housingLocationList: HousingLocation[] = [
     {
       id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
+      name: 'Emporium Hotel',
+      city: 'Lahore',
+      state: 'Pakistan',
       photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
       availableUnits: 4,
       wifi: true,
@@ -20,9 +20,9 @@ export class HousingService {
     },
     {
       id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
+      name: 'PC Hotel',
+      city: 'Lahore',
+      state: 'Pakistan',
       photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
       availableUnits: 0,
       wifi: false,
@@ -30,9 +30,9 @@ export class HousingService {
     },
     {
       id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
+      name: 'Serena Hotel',
+      city: 'Islamabad',
+      state: 'Capital',
       photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
       availableUnits: 1,
       wifi: false,
@@ -40,9 +40,9 @@ export class HousingService {
     },
     {
       id: 3,
-      name: 'Homesteady Housing',
-      city: 'Chicago',
-      state: 'IL',
+      name: 'Quetta Hotel',
+      city: 'Islamabad',
+      state: 'Capital',
       photo: `${this.baseUrl}/ian-macdonald-W8z6aiwfi1E-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
@@ -51,8 +51,8 @@ export class HousingService {
     {
       id: 4,
       name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
+      city: 'Rwp',
+      state: 'Punjab',
       photo: `${this.baseUrl}/krzysztof-hepner-978RAXoXnH4-unsplash.jpg`,
       availableUnits: 1,
       wifi: true,
@@ -60,9 +60,9 @@ export class HousingService {
     },
     {
       id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
+      name: 'Apartments',
+      city: 'Islamabad',
+      state: 'Punjab',
       photo: `${this.baseUrl}/r-architecture-JvQ0Q5IkeMM-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
@@ -70,9 +70,9 @@ export class HousingService {
     },
     {
       id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
+      name: 'Murree Hotels',
+      city: 'Murree',
+      state: 'Punjab',
       photo: `${this.baseUrl}/phil-hearing-IYfp2Ixe9nM-unsplash.jpg`,
       availableUnits: 5,
       wifi: true,
@@ -80,9 +80,9 @@ export class HousingService {
     },
     {
       id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
+      name: 'Housing Solutions',
+      city: 'Lahore',
+      state: 'Isb',
       photo: `${this.baseUrl}/r-architecture-GGupkreKwxA-unsplash.jpg`,
       availableUnits: 2,
       wifi: true,
@@ -90,8 +90,8 @@ export class HousingService {
     },
     {
       id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
+      name: 'Safe Towns',
+      city: 'Dubai',
       state: 'CA',
       photo: `${this.baseUrl}/saru-robert-9rP3mxf8qWI-unsplash.jpg`,
       availableUnits: 10,
@@ -101,8 +101,8 @@ export class HousingService {
     {
       id: 9,
       name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
+      city: 'Isb',
+      state: 'Pakistan',
       photo: `${this.baseUrl}/webaliser-_TPTXZd9mOo-unsplash.jpg`,
       availableUnits: 6,
       wifi: true,
@@ -116,5 +116,11 @@ export class HousingService {
 
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+  submitForm(firstName:string, lastName: string, email: string){
+    alert(firstName);
+    alert(lastName);
+    alert(email);
+    console.log(firstName, lastName, email)
   }
 }
